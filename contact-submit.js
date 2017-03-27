f7ce20b301f33aca40e39a61d4e6fca5537a3160
Nlanders87 <nlanders87@gmail.com>
2017-03-27 17:08:30 -0400

    Added javscript function to clear form on submit

--- contact-submit.js
+++ contact-submit.js
@@ -1,11 +1,10 @@
 //Logs out contact form entry to the console until server-side functionality can be added
 
-var nameInput = document.getElementById('name');
-document.querySelector('form.contact-info').addEventListener('submit', function (i) {
-    i.preventDefault();
-    console.log(nameInput.value);
-
-    
+var nameInput = document.getElementById('name'); // Creates variable that grabs the value of the input type
+document.querySelector('form.contact-info').addEventListener('submit', function (i) { // Function that on submit logs the value of the input to the console
+    i.preventDefault(); //Prevents page refresh
+    console.log(nameInput.value); //Logs value to the console
+    $('input[type="text"]').val(''); //Clears input on submit
 });
 
 
@@ -14,6 +13,7 @@ var mailInput = document.getElementById('mail');
 document.querySelector('form.contact-info').addEventListener('submit', function (i) {
     i.preventDefault();
     console.log(mailInput.value);
+    $('input[type="email"]').val('');
 });
 
 
@@ -22,6 +22,7 @@ var telInput = document.getElementById('telephone');
 document.querySelector('form.contact-info').addEventListener('submit', function (i) {
     i.preventDefault();
     console.log(telInput.value);
+    $('input[type="tel"]').val('');
 });
 
 
@@ -30,4 +31,6 @@ var inquiryInput = document.getElementById('user-inquiries');
 document.querySelector('form.contact-info').addEventListener('submit', function (i) {
     i.preventDefault();
     console.log(inquiryInput.value);
+    $('input[type="text"], textarea').val('');
 });
+
